20ab42b2e6ff710c5c400781f8c8ffc3f5e4a674
fsorgho <fadhe.sorgho@ayruu.com>
2020-01-30 17:32:18 +0100

    added package.json

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const { makeExecutableSchema } = require('graphql-tools');
+const request = require('./src/services/football.service.js');
 
 // Initialize the app
 const app = express();
 
 // The GraphQL endpoint
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphql', bodyParser.json(), graphqlExpress({ }));
 
 // GraphiQL, a visual editor for queries
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
+ request.getCompetitions();
 // Start the server
 app.listen(3000, () => {
   console.log('Go to http://localhost:3000/graphiql to run queries!');
